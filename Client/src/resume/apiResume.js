83fca6dc2f335a4388c8a760acb4b7a7035a1c7d
Lamooooo1418 <lamooooo1418@gmail.com>
2020-03-18 14:12:06 +0300

    Show

--- Client/src/resume/apiResume.js
+++ Client/src/resume/apiResume.js
@@ -1,13 +1,33 @@
 import apiUrl from "../apiConfig";
 import axios from "axios";
 
-//Get Profile
-export const getProfile = () => {
-  return axios.get(`${apiUrl}/profile`);
-};
+// //Get Profile
+// export const getProfile = () => {
+//   return axios.get(`${apiUrl}/profile`);
+// };
+
+
+export const signUp = credentials => {
+  return axios({
+    method: "POST",
+    url: apiUrl + "/sign-up",
+    data: {
+      credentials: {
+        firstName: credentials.firstName,
+        lastName: credentials.lastName,
+        username: credentials.username,
+        label: credentials.label,
+        phone: credentials.phone,
+        email: credentials.email,
+        password: credentials.password,
+        password_confirmation: credentials.passwordConfirmation
+      }
+    }
+  });
+}
 
 //Add Profile to the User
-export const addProfile = profile => {
+export const addresume = profile => {
   return axios.post(`${apiUrl}/profile`, { profile });
 };
 
