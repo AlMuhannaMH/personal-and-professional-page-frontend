9a393a3d444988ec1b2f8fdeff34fd31d2eef49a
Mohammed H. AlMuhanna <almuhannamh@outlook.com>
2020-03-20 00:44:47 +0300

    update apiConfig

--- Client/src/apiConfig.js
+++ Client/src/apiConfig.js
@@ -2,7 +2,7 @@ let apiUrl
 const expressPort = 5000
 const apiUrls = {
   development: `http://localhost:${expressPort}/api`,
-  production: `https://aqueous-atoll-85096.herokuapp.com/api`,
+  production: `https://dry-basin-76111.herokuapp.com/api`,
 }
 
 if (window.location.hostname === 'localhost') {
